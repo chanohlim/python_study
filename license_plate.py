9246944180e523ea7991a578a4505c36a4143432
Chan Lim <chanlim@hanyang.ac.kr>
2023-12-16 15:48:45 +0900

    .

--- license_plate.py
+++ license_plate.py
@@ -26,16 +26,14 @@ img = cv2.resize(img, (620,480) )
 
 
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to grey scale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #그레이스캐일로 변환
 
-gray = cv2.bilateralFilter(gray, 11, 17, 17) #Blur to reduce noise
+gray = cv2.bilateralFilter(gray, 11, 17, 17) #노이즈 감소
 
-edged = cv2.Canny(gray, 30, 200) #Perform Edge detection
+edged = cv2.Canny(gray, 30, 200) #경계 찾기
 
 
-# find contours in the edged image, keep only the largest
-
-# ones, and initialize our screen contour
+#대비를 찾고, 가장 대비가 큰 것만 keep
 
 cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -46,11 +44,9 @@ cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
 screenCnt = None
 
 
-# loop over our contours
+#대비 요소들에서 시퀸스 반복
 
 for c in cnts:
-
- # approximate the contour
 
  peri = cv2.arcLength(c, True)
 
@@ -111,8 +107,9 @@ Cropped = gray[topx:bottomx+1, topy:bottomy+1]
  
 
 
-#Read the number plate
+#번호판 사진에서 텍스트 추출
 text = pytesseract.image_to_string(Cropped, lang='kor', config='--psm 7 --oem 0')
+
 
 t_list = []
 
